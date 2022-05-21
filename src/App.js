dbebdbe62fc47b0561ebc16242ed4a4fe0a6b6b1
Jonathan Rafael Zanella <jonathanrz@MacBook-Pro-de-Jonathan.local>
2022-05-21 09:21:07 -0300

    Jonathan avatar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Avatar from "avataaars";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Avatar
+        avatarStyle="Transparent"
+        topType="ShortHairShortFlat"
+        accessoriesType="Prescription02"
+        hairColor="Blonde"
+        facialHairType="BeardLight"
+        facialHairColor="Blonde"
+        clotheType="ShirtScoopNeck"
+        clotheColor="Black"
+        eyeType="Wink"
+        eyebrowType="Default"
+        mouthType="Smile"
+        skinColor="Light"
+      />
     </div>
   );
 }
