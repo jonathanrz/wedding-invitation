579d94d00af8da1897a7c06ced11d09c892c12df
Jonathan Zanella <jonathanrafaelz@gmail.com>
2022-05-21 11:47:04 -0300

    images

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import styled from "styled-components";
 const Container = styled.div`
   display: grid;
   justify-content: center;
-  gap: 1rem;
   width: 100%;
+  height: 100vh;
+  position: relative;
+`;
+
+const Content = styled.div`
+  display: grid;
+  gap: 1rem;
 `;
 
 const AvatarContainer = styled.div`
@@ -13,7 +19,27 @@ const AvatarContainer = styled.div`
   margin: auto;
 `;
 
+const ThriathlonImg = styled.img`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 200px;
+`;
+
 const SallyImg = styled.img`
+  width: 200px;
+`;
+
+const KikiImg = styled.img`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+`;
+
+const RetsukoImg = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 200px;
 `;
 
@@ -51,41 +77,47 @@ function App() {
 
   return (
     <Container>
-      <Guests>{guests.join(" e ")}</Guests>
-      <Invitation>Vocês estão convidados para o nosso casamento</Invitation>
-      <AvatarContainer>
-        <Avatar
-          avatarStyle="Transparent"
-          topType="ShortHairShortFlat"
-          accessoriesType="Prescription02"
-          hairColor="Blonde"
-          facialHairType="BeardLight"
-          facialHairColor="Blonde"
-          clotheType="ShirtScoopNeck"
-          clotheColor="Black"
-          eyeType="Wink"
-          eyebrowType="Default"
-          mouthType="Smile"
-          skinColor="Light"
-        />
-        <SecondAvatar>
+      <KikiImg src={`${process.env.PUBLIC_URL}/images/kiki.png`} />
+      <RetsukoImg src={`${process.env.PUBLIC_URL}/images/retsuko.png`} />
+      <ThriathlonImg src={`${process.env.PUBLIC_URL}/images/triathlon.jpeg`} />
+
+      <Content>
+        <Guests>{guests.join(" e ")}</Guests>
+        <Invitation>Vocês estão convidados para o nosso casamento</Invitation>
+        <AvatarContainer>
           <Avatar
             avatarStyle="Transparent"
-            topType="LongHairStraightStrand"
-            accessoriesType="Blank"
-            hairColor="Brown"
-            facialHairType="Blank"
-            clotheType="Hoodie"
-            clotheColor="Gray02"
-            eyeType="Happy"
-            eyebrowType="RaisedExcited"
+            topType="ShortHairShortFlat"
+            accessoriesType="Prescription02"
+            hairColor="Blonde"
+            facialHairType="BeardLight"
+            facialHairColor="Blonde"
+            clotheType="ShirtScoopNeck"
+            clotheColor="Black"
+            eyeType="Wink"
+            eyebrowType="Default"
             mouthType="Smile"
             skinColor="Light"
           />
-        </SecondAvatar>
-        <SallyImg src={`${process.env.PUBLIC_URL}/images/sally.jpeg`} />
-      </AvatarContainer>
-      <Date>11 de junho de 2022 as 19h</Date>
+          <SecondAvatar>
+            <Avatar
+              avatarStyle="Transparent"
+              topType="LongHairStraightStrand"
+              accessoriesType="Blank"
+              hairColor="Brown"
+              facialHairType="Blank"
+              clotheType="Hoodie"
+              clotheColor="Gray02"
+              eyeType="Happy"
+              eyebrowType="RaisedExcited"
+              mouthType="Smile"
+              skinColor="Light"
+            />
+          </SecondAvatar>
+          <SallyImg src={`${process.env.PUBLIC_URL}/images/sally.jpeg`} />
+        </AvatarContainer>
+        <Date>11 de junho de 2022 as 19h</Date>
+      </Content>
     </Container>
   );
 }
